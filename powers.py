cde9e230cbabade8b1a638dc2fe78a9e97d7f77b
TrinidadFalcon <tfalcon@mail.austral.edu.ar>
2026-05-18 16:40:43 -0300

    Add files via upload

--- powers.py
+++ powers.py
@@ -1,24 +1,21 @@
-# Replace the "ANSWER HERE" for your answer
-
 def power(base, exp):
-    """
-    Retorna base elevado a exp usando un bucle for.
-    exp es siempre >= 0.
 
-    Ejemplo: power(2, 3) -> 8  (2*2*2)
-    """
 
-    potencia= 1
+    resultado = 1
+
     for i in range(exp):
-       potencia *= base
-    return potencia
+        resultado *= base
+
+    return resultado
+
 
 def sum_of_powers(base, max_exp):
 
+
     suma = 0
-    for i in range(0,max_exp):
-        suma += base**i
+
+    for i in range(max_exp + 1):
+        suma += power(base, i)
+
     return suma
-
-
 
